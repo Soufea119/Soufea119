0357f663f3ccd1dfe820607874a819968f89e2ec
Soufea119 <sofiahazrinsuffian@gmail.com>
2025-06-28 23:04:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ import { VRButton } from './libs/VRButton.js';
 import { CanvasUI } from './libs/CanvasUI.js';
 import { GazeController } from './libs/GazeController.js'
 import { XRControllerModelFactory } from './libs/three/jsm/XRControllerModelFactory.js';
-import { FBXLoader } from './libs/three/jsm/FBXLoader.js';
+import { GLTFLoader } from './libs/three/jsm/loaders/GLTFLoader.js';
+
 
 
 
