4e3d60332fca6577e39632b55e3b31453710628e
Soufea119 <sofiahazrinsuffian@gmail.com>
2025-06-29 18:48:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -72,26 +72,26 @@ audioLoader.load(
         console.error('An error occurred loading the audio:', err);
     }
 );
-
 		spawnAngels() {
-    const loader = new GLTFLoader();
-    loader.load(this.assetsPath + 'angel.glb', (gltf) => {
-        for (let i = 0; i < 20; i++) {
-            const angel = gltf.scene.clone();
-            const range = 30;
+		const loader = new GLTFLoader();
+		loader.load(this.assetsPath + 'angel.glb', (gltf) => {
+			for (let i = 0; i < 20; i++) {
+				const angel = gltf.scene.clone();
+				const range = 30;
 
-            angel.position.set(
-                (Math.random() - 0.5) * range,
-                0,
-                (Math.random() - 0.5) * range
-            );
+				angel.position.set(
+					(Math.random() - 0.5) * range,
+					0,
+					(Math.random() - 0.5) * range
+				);
 
-            angel.name = angel_${i};
-            this.scene.add(angel);
-            this.angels.push(angel);
-        }
-    });
-}
+				angel.name = `angel_${i}`;
+				this.scene.add(angel);
+				this.angels.push(angel);
+			}
+		});
+	}
+		
 		
                 this.scene.fog = new THREE.Fog(0x000000, 2, 20); // Closer and darker fog
 
@@ -124,6 +124,7 @@ audioLoader.load(
 		this.renderer.setSize( window.innerWidth, window.innerHeight );
 		this.renderer.outputEncoding = THREE.sRGBEncoding;
 		container.appendChild( this.renderer.domElement );
+		
         this.setEnvironment();
 	
         window.addEventListener( 'resize', this.resize.bind(this) );
